b4fc83ed0e979a57de7ff8ec1b82f064af5a5988
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2023-11-13 09:07:42 +0100

    fix: remove static method

--- App.php
+++ App.php
@@ -3,9 +3,9 @@ require_once './SendMail.php';
 
 class App
 {
-    public static function run(string $fileName)
+    public function run(string $fileName)
     {
-        $employees = self::getEmployeesFileLines($fileName);
+        $employees = $this->getEmployeesFileLines($fileName);
 
         if (!empty($employees)) {
             $firstLine = true;
@@ -14,9 +14,9 @@ class App
                 if ($firstLine) {
                     $firstLine = false;
                 } else {
-                    $employeeInfos = self::parseEmployeeInfos($employee);
+                    $employeeInfos = $this->parseEmployeeInfos($employee);
 
-                    self::sendEmployeeMailBirthday($employeeInfos);
+                    $this->sendEmployeeMailBirthday($employeeInfos);
                 }
             }
 
@@ -24,7 +24,7 @@ class App
         }
     }
 
-    public static function readEmployeesFile (string $fileName): string
+    public function readEmployeesFile (string $fileName): string
     {
         if (file_exists($fileName)) {
             echo "Reading file..." . PHP_EOL;
@@ -37,9 +37,9 @@ class App
         return '';
     }
 
-    public static function getEmployeesFileLines (string $fileName): array
+    public function getEmployeesFileLines (string $fileName): array
     {
-        $fileContent = self::readEmployeesFile($fileName);
+        $fileContent = $this->readEmployeesFile($fileName);
 
         if (empty($fileContent)) {
             echo "Error reading file " . PHP_EOL;
@@ -49,7 +49,7 @@ class App
         return explode(PHP_EOL, $fileContent);
     }
 
-    public static function parseEmployeeInfos (string $employeeInfos): array
+    public function parseEmployeeInfos (string $employeeInfos): array
     {
         $employeeInfos = explode(',', $employeeInfos);
 
@@ -66,7 +66,7 @@ class App
         return $employeeInfos;
     }
 
-    public static function parseEmployeeBirthDate (array $employeeInfos): array
+    public function parseEmployeeBirthDate (array $employeeInfos): array
     {
         $date = explode('/', $employeeInfos[2]);
 
@@ -79,17 +79,17 @@ class App
         return $date;
     }
 
-    public static function isEmployeeBirthDate (array $employeeBirthDate): bool
+    public function isEmployeeBirthDate (array $employeeBirthDate): bool
     {
         return (int)date('d') == (int)$employeeBirthDate[0] && (int)date('m') == (int)$employeeBirthDate[1];
     }
 
-    public static function sendEmployeeMailBirthday(array $employee)
+    public function sendEmployeeMailBirthday(array $employee)
     {
         if (!empty($employee)) {
-            $birthDate = self::parseEmployeeBirthDate($employee);
+            $birthDate = $this->parseEmployeeBirthDate($employee);
 
-            if (!empty($birthDate) && self::isEmployeeBirthDate($birthDate)) {
+            if (!empty($birthDate) && $this->isEmployeeBirthDate($birthDate)) {
                 SendMail::send(
                     $employee[3],
                     "Joyeux Anniversaire !",
@@ -100,4 +100,4 @@ class App
     }
 }
 
-App::run(__DIR__ . DIRECTORY_SEPARATOR . 'employees.txt');
+(new App())->run(__DIR__ . DIRECTORY_SEPARATOR . 'employees.txt');
