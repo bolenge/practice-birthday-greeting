8932395ea1a08c0466e81bd2aa4cd9ba9050c23c
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2023-11-13 08:46:52 +0100

    refactor: extract logic in methods

--- App.php
+++ App.php
@@ -5,43 +5,97 @@ class App
 {
     public static function run(string $fileName)
     {
+        $employees = self::getEmployeesFileLines($fileName);
+
+        if (!empty($employees)) {
+            $firstLine = true;
+
+            foreach ($employees as $employee) {
+                if ($firstLine) {
+                    $firstLine = false;
+                } else {
+                    $employeeInfos = self::parseEmployeeInfos($employee);
+
+                    self::sendEmployeeMailBirthday($employeeInfos);
+                }
+            }
+
+                echo "Batch job done." . PHP_EOL;
+        }
+    }
+
+    public static function readEmployeesFile (string $fileName): string
+    {
         if (file_exists($fileName)) {
             echo "Reading file..." . PHP_EOL;
-            $firstLine = true;
-            $content = explode(PHP_EOL, file_get_contents($fileName));
-            if (!empty($content)) {
-                foreach ($content as $line) {
-                    if ($firstLine) {
-                        $firstLine = false;
-                    } else {
-                        $tokens = explode(',', $line);
-                        foreach ($tokens as $i => $token) {
-                            $tokens[$i] = trim($token);
-                        }
-                        if (count($tokens) == 4) {
-                            $date = explode('/', $tokens[2]);
-                            if (count($date) == 3) {
-                                if ((int)date('d') == (int)$date[0] && (int)date('m') == (int)$date[1]) {
-                                    SendMail::send(
-                                        $tokens[3],
-                                        "Joyeux Anniversaire !",
-                                        "Bonjour " . $tokens[0] . ",\nJoyeux Anniversaire !\nA bientôt,"
-                                    );
-                                }
-                            } else {
-                                echo "Cannot read birthdate for " . $tokens[0] . " " . $tokens[1] . PHP_EOL;
-                            }
-                        } else {
-                            echo "Invalid file format" . PHP_EOL;
-                        }
-                    }
-                }
-                echo "Batch job done." . PHP_EOL;
-            } else {
-                echo "Error reading file '" . $fileName . "'" . PHP_EOL;
+
+            return file_get_contents($fileName);
+        }
+
+        echo "Unable to open file '" . $fileName . "'" . PHP_EOL;
+
+        return '';
+    }
+
+    public static function getEmployeesFileLines (string $fileName): array
+    {
+        $fileContent = self::readEmployeesFile($fileName);
+
+        if (empty($fileContent)) {
+            echo "Error reading file " . PHP_EOL;
+            return [];
+        }
+
+        return explode(PHP_EOL, $fileContent);
+    }
+
+    public static function parseEmployeeInfos (string $employeeInfos): array
+    {
+        $employeeInfos = explode(',', $employeeInfos);
+
+        foreach ($employeeInfos as $i => $token) {
+            $employeeInfos[$i] = trim($token);
+        }
+
+        if (count($employeeInfos) !== 4) {
+            echo "Invalid file format" . PHP_EOL;
+
+            return [];
+        }
+
+        return $employeeInfos;
+    }
+
+    public static function parseEmployeeBirthDate (array $employeeInfos): array
+    {
+        $date = explode('/', $employeeInfos[2]);
+
+        if (count($date) !== 3) {
+            echo "Cannot read birthdate for " . $employeeInfos[0] . " " . $employeeInfos[1] . PHP_EOL;
+
+            return [];
+        }
+
+        return $date;
+    }
+
+    public static function isEmployeeBirthDate (array $employeeBirthDate): bool
+    {
+        return (int)date('d') == (int)$employeeBirthDate[0] && (int)date('m') == (int)$employeeBirthDate[1];
+    }
+
+    public static function sendEmployeeMailBirthday(array $employee)
+    {
+        if (!empty($employee)) {
+            $birthDate = self::parseEmployeeBirthDate($employee);
+
+            if (!empty($birthDate) && self::isEmployeeBirthDate($birthDate)) {
+                SendMail::send(
+                    $employee[3],
+                    "Joyeux Anniversaire !",
+                    "Bonjour " . $employee[0] . ",\nJoyeux Anniversaire !\nA bientôt,"
+                );
             }
-        } else {
-            echo "Unable to open file '" . $fileName . "'" . PHP_EOL;
         }
     }
 }
